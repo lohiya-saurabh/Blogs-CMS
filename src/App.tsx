bbc3034b1461f1ef059ec377b537c0311f0484c9
Saurabh <saurabh.lohiya@rowisto.com>
2023-04-20 12:57:36 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.scss'
+import Navbar from './components/navbar/navbar'
+
+function App() {
+
+  return (
+    <div className="page content-center bg-blue-100">
+      <Navbar></Navbar>
+      <section className="container">
+        <div className="">
+          <p className="text-5xl">
+            Responsiveness can be a very shaky subject
+          </p>
+          <p className="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque quo, qui harum dignissimos ipsam cupiditate
+            alias sit expedita? Quas illum mollitia quam, pariatur ipsum reiciendis qui natus distinctio omnis nam!
+          </p>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <section className="container">
+      <div className="nav w-full flex bg-red-50">
+        <div className="nav-item self-start">Log2n</div>
+        <div className="nav-item self-end">Hello</div>
+        <div className="nav-item">Hi</div>
+      </div>
+    </section>
+  )
+}
+
+export default Navbar
